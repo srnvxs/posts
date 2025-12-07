378652833a83c28f003ea7731022fc09070e931c
[Oʀᴇᴡᴀ_Sʀɪɴɪᴠᴀs]²ᵘ <104828717+srnvxs@users.noreply.github.com>
2025-12-07 10:59:02 +0530

    Update anilist.py

--- anilist.py
+++ anilist.py
@@ -4,14 +4,30 @@ API_URL = "https://animmes2uapi.vercel.app/anilist"
 
 def get_anime(query: str):
     try:
-        r = requests.get(API_URL, params={"query": query}, timeout=10)
-        r.raise_for_status()
-        j = r.json()
+        r = requests.get(
+            API_URL,
+            params={"query": query},
+            timeout=15,
+            headers={"User-Agent": "Mozilla/5.0"}
+        )
 
-        if j.get("success") and j.get("anime"):
-            return j["anime"]
+        if r.status_code != 200:
+            print("HTTP Error:", r.status_code)
+            print(r.text)
+            return None
+
+        try:
+            data = r.json()
+        except Exception:
+            print("❌ Not JSON Response:")
+            print(r.text)
+            return None
+
+        if data.get("success") and data.get("anime"):
+            return data["anime"]
 
         return None
+
     except Exception as e:
-        print(e)
+        print("Request Failed:", e)
         return None
