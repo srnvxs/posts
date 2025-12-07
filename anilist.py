4590c0b29dbf0b27932664e8ce111a5546f13db2
[Oʀᴇᴡᴀ_Sʀɪɴɪᴠᴀs]²ᵘ <104828717+srnvxs@users.noreply.github.com>
2025-12-07 10:57:17 +0530

    Update anilist.py

--- anilist.py
+++ anilist.py
@@ -2,10 +2,16 @@ import requests
 
 API_URL = "https://animmes2uapi.vercel.app/anilist"
 
-def get_anime(name: str):
+def get_anime(query: str):
     try:
-        r = requests.get(API_URL, params={"query": name}, timeout=10)
-        data = r.json()
-        return data.get("data")
-    except Exception:
+        r = requests.get(API_URL, params={"query": query}, timeout=10)
+        r.raise_for_status()
+        j = r.json()
+
+        if j.get("success") and j.get("anime"):
+            return j["anime"]
+
+        return None
+    except Exception as e:
+        print(e)
         return None
